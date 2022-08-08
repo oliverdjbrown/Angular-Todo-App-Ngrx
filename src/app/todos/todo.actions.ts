707beebe33025ae9fa7fa862ed8b36f57b1751b7
Oliver Brown <oliverbrodev@gmail.com>
2022-08-08 14:28:06 -0400

    add actions of delete and edit

--- src/app/todos/todo.actions.ts
+++ src/app/todos/todo.actions.ts
@@ -1,4 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 
-export const create = createAction('[TODO] Create Todo', props<{text: string}>());
-export const toggle = createAction('[TODO] Toggle Todo', props<{id: number}>());
+export const createTodo = createAction('[TODO] Create Todo', props<{text: string}>());
+export const editTodo = createAction('[TODO] Edit Todo', props<{id: number, text: string}>());
+export const deleteTodo = createAction('[TODO] Edit Todo', props<{id: number}>());
+export const toggleTodo = createAction('[TODO] Toggle Todo', props<{id: number}>());
