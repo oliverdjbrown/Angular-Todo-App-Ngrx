a49323e5a67ba44cd1ec9a8da96fddaed331c53a
Oliver Brown <oliverbrodev@gmail.com>
2022-08-08 14:28:32 -0400

    add operations of delete and edit

--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -1,6 +1,6 @@
 import { createReducer, on } from '@ngrx/store';
 import { Todo } from './models/todo.model';
-import { create, toggle } from './todo.actions';
+import * as actions from './todo.actions';
 
 export const initialState: Todo[] = [
   new Todo('Salvar el mundo'),
@@ -11,8 +11,14 @@ export const initialState: Todo[] = [
 
 export const todoReducer = createReducer(
   initialState,
-  on(create, (state, { text }) => [...state, new Todo(text)]),
-  on(toggle, (state, { id }) =>
+  on(actions.createTodo, (state, { text }) => [...state, new Todo(text)]),
+  on(actions.deleteTodo, (state, { id }) => state.filter(todo => todo.id !== id)),
+  on(actions.editTodo, (state, { id, text }) =>
+    state.map((todo) =>
+      todo.id === id ? { ...todo, text } : todo
+    )
+  ),
+  on(actions.toggleTodo, (state, { id }) =>
     state.map((todo) =>
       todo.id === id ? { ...todo, completed: !todo.completed } : todo
     )
