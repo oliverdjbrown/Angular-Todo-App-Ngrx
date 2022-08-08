3977082eee384e176679f3559ad2e67d057731c3
Oliver Brown <oliverbrodev@gmail.com>
2022-08-08 10:22:43 -0400

    add component logic

--- src/app/todos/todo-add/todo-add.component.ts
+++ src/app/todos/todo-add/todo-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import * as actions from '../todo.actions';
 
 @Component({
   selector: 'app-todo-add',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoAddComponent implements OnInit {
 
-  constructor() { }
+  todo = new FormControl('',Validators.required);
+  
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+  }
+
+  addTodo(): void {
+    if(this.todo.invalid) return; 
+    this.store.dispatch(actions.create({text: this.todo.value!}));
+    this.clearTodoField();
+  }
+
+  clearTodoField(): void {
+    this.todo.reset();
   }
 
 }
