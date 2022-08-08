ebea1808fd2e655017633dcae9e8b2eaaaaddfc7
Oliver Brown <oliverbrodev@gmail.com>
2022-08-08 10:19:58 -0400

    install and configure ngrx  and devtolls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
 import { StoreModule } from '@ngrx/store';
+import { todoReducer } from './todos/todo.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,9 +17,10 @@ import { StoreModule } from '@ngrx/store';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule,    
     TodoModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({todos: todoReducer}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
