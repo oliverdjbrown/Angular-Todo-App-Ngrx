1711bd19249418cccce90815b1103da228613ced
Oliver Brown <oliverbrodev@gmail.com>
2022-08-09 13:32:42 -0400

    add new action

--- src/app/todos/todo.actions.ts
+++ src/app/todos/todo.actions.ts
@@ -4,3 +4,4 @@ export const createTodo = createAction('[TODO] Create Todo', props<{text: string
 export const editTodo = createAction('[TODO] Edit Todo', props<{id: number, text: string}>());
 export const deleteTodo = createAction('[TODO] Edit Todo', props<{id: number}>());
 export const toggleTodo = createAction('[TODO] Toggle Todo', props<{id: number}>());
+export const toggleAllTodos = createAction('[TODO] Toggle All Todos', props<{completed: boolean}>());
