55dc504320f5cadf15c10ad570243c3e38094e5b
Oliver Brown <oliverbrodev@gmail.com>
2022-08-09 13:32:12 -0400

    add logic to the component

--- src/app/todos/todo-page/todo-page.component.ts
+++ src/app/todos/todo-page/todo-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import * as actions from '../todo.actions';
+
 
 @Component({
   selector: 'app-todo-page',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoPageComponent implements OnInit {
 
-  constructor() { }
+  completed: boolean = false;
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+  }
+
+  toggleAll(): void {
+    this.completed = !this.completed;
+    this.store.dispatch(actions.toggleAllTodos({completed: this.completed}));
   }
 
 }
