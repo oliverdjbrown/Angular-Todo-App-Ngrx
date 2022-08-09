c96bf1fbfcfdc8cd2807cefcfc21001d7be3d67e
Oliver Brown <oliverbrodev@gmail.com>
2022-08-09 13:31:49 -0400

    add logic to the component

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { filterTypes, setFilter } from 'src/app/filter/filter.actions';
 
 @Component({
   selector: 'app-todo-footer',
   templateUrl: './todo-footer.component.html',
-  styleUrls: ['./todo-footer.component.scss']
+  styleUrls: ['./todo-footer.component.scss'],
 })
 export class TodoFooterComponent implements OnInit {
+  currentFilter = 'Todos';
+  filters: filterTypes[] = ['Todos', 'Completados', 'Pendientes'];
+  pending: number = 0;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store.subscribe((state) => {
+      this.currentFilter = state.filter;
+      this.pending = state.todos.filter((todo) => !todo.completed).length;
+    });
   }
 
+  setFilter(filter: filterTypes): void {
+    this.store.dispatch(setFilter({ filter }));
+  }
 }
