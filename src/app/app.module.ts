2c934a9d30dea883c5f32cc18f33a37f04a6a34c
Oliver Brown <oliverbrodev@gmail.com>
2022-08-09 13:30:59 -0400

    move  Store reducers imports to avoid over size file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { appReducers } from './app.reducer';
+
 import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
-import { StoreModule } from '@ngrx/store';
-import { todoReducer } from './todos/todo.reducer';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 
 @NgModule({
@@ -19,7 +21,7 @@ import { environment } from '../environments/environment';
     BrowserModule,
     AppRoutingModule,    
     TodoModule,
-    StoreModule.forRoot({todos: todoReducer}, {}),
+    StoreModule.forRoot(appReducers),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
