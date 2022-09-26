f95e1b018f73774a7e3e27410e507e2e69a7bfa9
Oliver Brown <oliverbrodev@gmail.com>
2022-09-26 12:12:11 -0400

    re order and export elemnts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilterPipe } from './pipes/filter.pipe';
+import { TodoInputComponent } from './components/todo-input/todo-input.component';
+import { TodoButtonComponent } from './components/todo-button/todo-button.component';
+
+const components = [
+  TodoInputComponent, 
+  TodoButtonComponent
+];
+
+const pipes = [FilterPipe];
+
 
 @NgModule({
-  declarations: [FilterPipe],
-  imports: [
-    CommonModule,    
+  declarations: [
+    ...components,
+    ...pipes
   ],
-  exports: [FilterPipe]
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    ...components,
+    ...pipes
+  ]
 })
 export class SharedModule { }
